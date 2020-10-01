47020fae95bfaa8f9e0c9b9cd881fa3ef0a238fa
Marek Szkudelski <marek.szkudelski@sonalake.com>
2020-10-01 20:31:06 +0200

    random string added

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 const http = require('http');
+const random = require('randomstring');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Set-Cookie', `token=asddddsadsa43121; Max-Age=3600; HttpOnly;`);
+    res.setHeader('Set-Cookie', `token=${random.generate(32)}; Max-Age=3600; HttpOnly;`);
     res.end('Hello World');
 });
 
